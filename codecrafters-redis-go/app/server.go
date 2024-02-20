7c7de679146b141eccaec54bc8b5df664daa5b2a
Madhan <madhankumaravelu93@gmail.com>
2024-02-19 22:19:55 -0600

    feat: Respond to multiple PINGs

--- codecrafters-redis-go/app/server.go
+++ codecrafters-redis-go/app/server.go
@@ -8,29 +8,49 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
+	// Listen for incoming connections
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
+	// Ensure we teardown the server when the program exits
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 6379")
+
 	for {
 		log.Println("Waiting for a client to connect")
-
 		conn, err := listener.Accept()
-
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go do(conn)
+
+		// Handle client connection
+		go handleClient(conn)
 	}
 }
 
-func do(conn net.Conn) {
-	log.Println("processing the request")
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+	defer conn.Close()
+
+	// Read data
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading request", err.Error())
+			return
+		}
+
+		log.Printf("Received %d bytes", n)
+		log.Printf("Received the following data: %s", string(buf[:n]))
+
+		// Write response data back
+		log.Println("Processing the request")
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
